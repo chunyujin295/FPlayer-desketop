4f4bd4be393541ca83a5b51e3003ef89e11673f7
chenyujin <3029795434@qq.com>
2026-02-28 11:43:11 +0800

    build: 增加后端拆分

---
+++ backend/net_qt6/include/fplayer/backend/net_qt6/streamqt6.h
@@ -0,0 +1,21 @@
+/*************************************************
+  * 描述：
+  *
+  * File：streamqt6.h
+  * Author：chenyujin@mozihealthcare.cn
+  * Date：2026/2/28
+  * Update：
+  * ************************************************/
+#ifndef FPLAYER_DESKETOP_STREAMQT6_H
+#define FPLAYER_DESKETOP_STREAMQT6_H
+#include <fplayer/backend/net_qt6/export.h>
+namespace fplayer
+{
+    class FPLAYER_BACKEND_NET_QT6_EXPORT StreamQt6
+    {
+    public:
+        StreamQt6();
+    };
+}
+
+#endif //FPLAYER_DESKETOP_STREAMQT6_H
